fb341195d02fb5d7905bc28e5e74043bd6023199
JoaoMachado22 <joao.vicente@evolutix.com.br>
2023-12-03 16:25:27 -0300

    fix some changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,14 @@
 module.exports = {
   dialect: "postgres",
-  host: "viaduct.proxy.rlwy.net",
+  host: "roundhouse.proxy.rlwy.net",
   username: "postgres",
-  password: "ec*662bF6A-B3g25AcB-a415326DafaA",
-  database: "postgres",
+  password: "Cd-DaC2*15446136E3Df1b6AE2f-d3fG",
+  database: "railway",
+  port: 59676,
   define: {
     timestamps: true,
-    underscored: true,
-  },
+    underscored: true
+  }
 };
 
 //create_at, update_at
