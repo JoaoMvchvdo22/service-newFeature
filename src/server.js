7ec5050525f80282dc5b6c2e633139782985b0c1
JoaoMachado22 <joao.vicente@evolutix.com.br>
2023-12-02 23:42:39 -0300

    fixing server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 require("./database");
 
 const app = express();
+
+let corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
